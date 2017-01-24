36b34bbeed2a6116093b17d49135d20f900e4785
Michael Armbruster <armbrust@fim.uni-passau.de>
2017-01-24 13:57:10 +0100

    Forgot those essential pre-compiler instructions

--- source/3dsexit.h
+++ source/3dsexit.h
@@ -1,3 +1,6 @@
+#ifndef _3DSEXIT_H_
+#define _3DSEXIT_H_
+
 #include <3ds.h>
 
 aptHookCookie hookCookie;
@@ -5,3 +8,5 @@ int appExiting = 0;
 
 void setExitFlag(APT_HookType hook, void* param);
 void enableExitHook();
+
+#endif
