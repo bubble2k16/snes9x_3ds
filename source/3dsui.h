f248ce5fa314372dc0bd3c45a630d84b400fd65f
Admiral H. Curtiss <pikachu025@gmail.com>
2017-03-22 22:50:20 +0100

    Non-modified char* parameters should be const.

--- source/3dsui.h
+++ source/3dsui.h
@@ -16,8 +16,8 @@ int ui3dsApplyAlphaToColor(int color, float alpha);
 void ui3dsDrawRect(int x0, int y0, int x1, int y1);
 void ui3dsDrawRect(int x0, int y0, int x1, int y1, int color, float alpha = 1.0f);
 
-void ui3dsDrawStringWithWrapping(int x0, int y0, int x1, int y1, int color, int horizontalAlignment, char *buffer);
-void ui3dsDrawStringWithNoWrapping(int x0, int y0, int x1, int y1, int color, int horizontalAlignment, char *buffer);
+void ui3dsDrawStringWithWrapping(int x0, int y0, int x1, int y1, int color, int horizontalAlignment, const char *buffer);
+void ui3dsDrawStringWithNoWrapping(int x0, int y0, int x1, int y1, int color, int horizontalAlignment, const char *buffer);
 
 void ui3dsCopyFromFrameBuffer(uint16 *destBuffer);
 void ui3dsBlitToFrameBuffer(uint16 *srcBuffer, float alpha = 1.0f);
